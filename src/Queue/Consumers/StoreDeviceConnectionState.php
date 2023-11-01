b43d002242ef1fdbd166e3cfc0cb62b0d6707bf6
GitHub <action@github.com>
2023-11-01 18:57:24 +0000

    [virtual-connector] Virtual devices connector (#153)

--- src/Queue/Consumers/StoreDeviceConnectionState.php
+++ src/Queue/Consumers/StoreDeviceConnectionState.php
@@ -26,10 +26,8 @@ use FastyBird\Module\Devices\Entities as DevicesEntities;
 use FastyBird\Module\Devices\Exceptions as DevicesExceptions;
 use FastyBird\Module\Devices\Models as DevicesModels;
 use FastyBird\Module\Devices\Queries as DevicesQueries;
-use FastyBird\Module\Devices\States as DevicesStates;
 use FastyBird\Module\Devices\Utilities as DevicesUtilities;
 use Nette;
-use Nette\Utils;
 
 /**
  * Store device connection state message consumer
@@ -116,12 +114,7 @@ final class StoreDeviceConnectionState implements Queue\Consumer
 					$findDevicePropertiesQuery,
 					DevicesEntities\Devices\Properties\Dynamic::class,
 				) as $property) {
-					$this->devicePropertiesStatesManager->setValue(
-						$property,
-						Utils\ArrayHash::from([
-							DevicesStates\Property::VALID_KEY => false,
-						]),
-					);
+					$this->devicePropertiesStatesManager->setValidState($property, false);
 				}
 
 				$findChannelsQuery = new Queries\FindChannels();
@@ -137,12 +130,7 @@ final class StoreDeviceConnectionState implements Queue\Consumer
 						$findChannelPropertiesQuery,
 						DevicesEntities\Channels\Properties\Dynamic::class,
 					) as $property) {
-						$this->channelPropertiesStatesManager->setValue(
-							$property,
-							Utils\ArrayHash::from([
-								DevicesStates\Property::VALID_KEY => false,
-							]),
-						);
+						$this->channelPropertiesStatesManager->setValidState($property, false);
 					}
 				}
 			}
@@ -175,12 +163,7 @@ final class StoreDeviceConnectionState implements Queue\Consumer
 							$findDevicePropertiesQuery,
 							DevicesEntities\Devices\Properties\Dynamic::class,
 						) as $property) {
-							$this->devicePropertiesStatesManager->setValue(
-								$property,
-								Utils\ArrayHash::from([
-									DevicesStates\Property::VALID_KEY => false,
-								]),
-							);
+							$this->devicePropertiesStatesManager->setValidState($property, false);
 						}
 
 						$findChannelsQuery = new Queries\FindChannels();
@@ -199,12 +182,7 @@ final class StoreDeviceConnectionState implements Queue\Consumer
 								$findChannelPropertiesQuery,
 								DevicesEntities\Channels\Properties\Dynamic::class,
 							) as $property) {
-								$this->channelPropertiesStatesManager->setValue(
-									$property,
-									Utils\ArrayHash::from([
-										DevicesStates\Property::VALID_KEY => false,
-									]),
-								);
+								$this->channelPropertiesStatesManager->setValidState($property, false);
 							}
 						}
 					}
